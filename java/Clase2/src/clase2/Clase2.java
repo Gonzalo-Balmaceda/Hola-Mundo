7c0570435b8ce829a3fc80f7441a89f40392fa10
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2023-05-10 20:41:22 -0300

    Se creo una carpeta para la guardar la Clase2

---
+++ java/Clase2/src/clase2/Clase2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author wwwgo
+ */
+public class Clase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        
+        //Tipo String
+        String miVariableCadena = "Bienvenido";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
+    }
+    
+}
